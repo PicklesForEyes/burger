d851b2c607757467664b0bdf6f30788ad75eb3e1
Benjamin Lawson <lawbe101@yahoo.com>
2017-12-08 16:59:08 -0500

    fixed database name

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: 'us-cdbr-iron-east-05.cleardb.net',
   user: 'bfc76c0550fc89',
   password: '8832914f',
-  database: 'burgers_db'
+  database: 'heroku_c4e639b61a287a1'
 })
 
 connection.connect((err) => {
